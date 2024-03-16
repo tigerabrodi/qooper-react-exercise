89c31138fca97012c09d9e65c2de1e653598d08e
Tiger Abrodi <tigerabrodi@gmail.com>
2024-03-16 13:53:46 +0100

    feat: implement navigation component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./button";
 export * from "./typography";
 export * from "./input";
+export * from "./navigation";
---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { Button, Typography } from "..";
+import { useUser } from "../../hooks";
+
+const Nav = styled.nav`
+  width: 100%;
+  padding: 16px;
+  background-color: ${({ theme }) => theme.colors.grayLight};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grayMedium};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  column-gap: 32px;
+  align-items: center;
+  justify-content: end;
+`;
+
+export function Navigation() {
+  const { currentUser, logout } = useUser();
+
+  return (
+    <Nav>
+      <Wrapper>
+        <Typography variant="Text1">
+          Welcome, {currentUser?.firstName}
+        </Typography>
+        <Button onClick={logout} type="button">
+          LOGOUT
+        </Button>
+      </Wrapper>
+    </Nav>
+  );
+}
